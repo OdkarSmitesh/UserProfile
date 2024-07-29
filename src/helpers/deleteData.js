f63303eae8380d341384e3764a4f4472c6387fe6
OdkarSmitesh <odkarsmitesh@gmail.com>
2024-07-29 11:12:29 +0530

    removing _

--- src/helpers/deleteData.js
+++ src/helpers/deleteData.js
@@ -1,7 +1,7 @@
 import { toast } from "react-toastify";
 
 export default function deleteData(indexNumber, userData, setUserData) {
-  let dataLeft = userData.filter((_, i) => i !== indexNumber);
+  let dataLeft = userData.filter((i) => i !== indexNumber);
   setUserData(dataLeft);
   toast.success("Data Deleted successfully..");
 }
